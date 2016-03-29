7f07d44342ce54a2ec02cd1454b20bce667fa5db
Andrey K (home) <freazeek@mail.ru>
2016-03-29 22:25:25 +0300

    adding action

--- ListOfDeal/Action.cs
+++ ListOfDeal/Action.cs
@@ -17,16 +17,16 @@ namespace ListOfDeal
         public int Id { get; set; }
         public string Name { get; set; }
         public int ProjectId { get; set; }
-        public Nullable<int> ActionTriggerId { get; set; }
-        public int Status { get; set; }
         public System.DateTime DateCreated { get; set; }
         public Nullable<System.DateTime> ScheduledTime { get; set; }
         public Nullable<int> DelegatedTo { get; set; }
         public string Comment { get; set; }
+        public int StatusId { get; set; }
+        public Nullable<int> TriggerId { get; set; }
     
-        public virtual ActionStatus ActionStatus { get; set; }
-        public virtual ActionTrigger ActionTrigger { get; set; }
         public virtual DelegatePerson DelegatePerson { get; set; }
         public virtual Project Project { get; set; }
+        public virtual ActionStatus ActionStatus { get; set; }
+        public virtual ActionTrigger ActionTrigger { get; set; }
     }
 }
--- ListOfDeal/Classes/MainViewModel.cs
+++ ListOfDeal/Classes/MainViewModel.cs
@@ -16,7 +16,13 @@ namespace ListOfDeal {
         }
         ListOfDealBaseEntities generalEntity;
         ICommand _addNewProjectCommand;
+        ICommand _addActionCommand;
+
+     
         Project _currentProject;
+        Action _currentAction;
+
+  
         public ICommand AddNewProjectCommand {
             get {
                 if (_addNewProjectCommand == null)
@@ -25,25 +31,49 @@ namespace ListOfDeal {
                 return _addNewProjectCommand; }
         }
 
-    
+        public ICommand AddActionCommand {
+            get {
+                if (_addActionCommand == null)
+                    _addActionCommand = new DelegateCommand(AddAction);
+                return _addActionCommand; }
+        }
+
+   
 
         public ObservableCollection<Project> Projects { get; set; }
+        public Project SelectedProject { get; set; }
         public Project CurrentProject {
             get { return _currentProject; }
             set { _currentProject = value;
             RaisePropertyChanged("CurrentProject");
             }
         }
+        public Action CurrentAction {
+            get { return _currentAction; }
+            set { _currentAction = value;
+            RaisePropertyChanged("CurrentAction");
+            }
+        }
         public ObservableCollection<ProjectType> ProjectTypes { get; set; }
         public ObservableCollection<ProjectStatus> ProjectStatuses { get; set; }
+        public ObservableCollection<ActionTrigger> ActionTriggers { get; set; }
+        public ObservableCollection<ActionStatus> ActionStatuses { get; set; }
+        public ObservableCollection<DelegatePerson> DelegatePersons { get; set; }
         
         void InitializeData() {
             ConnectToDataBase();
             Projects = new ObservableCollection<Project>(generalEntity.Projects);
             ProjectTypes = new ObservableCollection<ProjectType>(generalEntity.ProjectTypes);
             ProjectStatuses = new ObservableCollection<ProjectStatus>(generalEntity.ProjectStatuses);
+            ActionTriggers = new ObservableCollection<ActionTrigger>(generalEntity.ActionTriggers);
+            ActionStatuses = new ObservableCollection<ActionStatus>(generalEntity.ActionStatuses);
+            DelegatePersons = new ObservableCollection<DelegatePerson>(generalEntity.DelegatePersons);
             CreateNewProject();
+            CreateNewAction();
+
         }
+
+     
 
         private void ConnectToDataBase() {
            
@@ -61,6 +91,12 @@ namespace ListOfDeal {
             CurrentProject.TypeId = 1;
             CurrentProject.StatusId = 1;
         }
+        private void CreateNewAction() {
+            CurrentAction = generalEntity.Actions.Create();
+            CurrentAction.StatusId = 1;
+            
+
+        }
         private void AddNewProject() {
             CurrentProject.DateCreated = DateTime.Now;
             generalEntity.Projects.Add(CurrentProject);
@@ -68,10 +104,20 @@ namespace ListOfDeal {
             generalEntity.SaveChanges();
             CreateNewProject();
         }
-
+        private void AddAction() {
+            CurrentAction.DateCreated = DateTime.Now;
+            SelectedProject.Actions.Add(CurrentAction);
+           
+            generalEntity.SaveChanges();
+            SelectedProject.RaisePropertyChanged("ActionsList");
+            CreateNewAction();
+        }
         internal void Test() {
             var a = generalEntity.Actions.Create();
             a.Name = "testaction";
+            a.DateCreated = DateTime.Now;
+            a.StatusId = 1;
+            a.TriggerId = 1;
             Projects[0].Actions.Add(a);
 
             generalEntity.SaveChanges();
@@ -82,5 +128,21 @@ namespace ListOfDeal {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+    }
+
+    public partial class Project:INotifyPropertyChanged {
+        public ObservableCollection<Action> ActionsList {
+            get {
+                return new ObservableCollection<Action>(this.Actions);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
     }
 }

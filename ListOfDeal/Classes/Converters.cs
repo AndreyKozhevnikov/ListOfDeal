dddefd934c0f9ca5136ac742d1dfe56676d308fc
Andrey K (home) <freazeek@mail.ru>
2016-04-11 08:53:27 +0300

    Export2

---
+++ ListOfDeal/Classes/Converters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ListOfDeal {
+    class CustomDisplayTextConverter :MarkupExtension,IValueConverter{
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (value == null)
+                return value;
+            string st = value.ToString();
+            if (st == "Trigger Id: ")
+                return "Trigger Id: Empty trigger";
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class custConverter :MarkupExtension, IValueConverter, IMultiValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+           
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return value;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return values[0];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}

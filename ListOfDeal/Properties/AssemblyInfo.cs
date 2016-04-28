00e35323778f14ead7b513fa39da6a749e9cc77d
Andrey K (home) <freazeek@mail.ru>
2016-04-28 21:19:43 +0300

    chart - fix label

--- ListOfDeal/Properties/AssemblyInfo.cs
+++ ListOfDeal/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("16.4.27.1836")]
-[assembly: AssemblyVersion("16.4.27.1836")]
-[assembly: AssemblyFileVersion("16.4.27.1836")]
+// [assembly: AssemblyVersion("16.4.28.2119")]
+[assembly: AssemblyVersion("16.4.28.2119")]
+[assembly: AssemblyFileVersion("16.4.28.2119")]

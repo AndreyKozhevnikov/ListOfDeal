8b46553c143e87921eb3e3fd7db2fd45b6a5a87a
kozhevnikov.andrey <andrey.kozhevnikov@devexpress.com>
2017-02-20 16:52:17 +0300

    table plan - done

--- ListOfDeal/Classes/WeekStatisticViewModel.cs
+++ ListOfDeal/Classes/WeekStatisticViewModel.cs
@@ -26,9 +26,34 @@ namespace ListOfDeal.Classes {
                 return _getItemsCommand;
             }
         }
-
+  
         void GetItems() {
             WeekRecords =new ObservableCollection<WeekRecord>( MainViewModel.DataProvider.GetWeekRecords());
+        }
+        ICommand _markItemsCompleteCommand;
+        public ICommand MarkItemsCompleteCommand {
+            get {
+                if (_markItemsCompleteCommand == null)
+                    _markItemsCompleteCommand = new DelegateCommand(MarkItemsComplete);
+                return _markItemsCompleteCommand;
+            }
+        }
+        void MarkItemsComplete() {
+            var wRecords = MainViewModel.DataProvider.GetWeekRecords();
+            foreach (var wr in wRecords) {
+                string wId = wr.WeekId;
+
+                int m = int.Parse(wId.Substring(0, 2));
+                int d = int.Parse(wId.Substring(2, 2));
+                int y = int.Parse(wId.Substring(4, 4));
+                DateTime wDt = new DateTime( y, m, d);
+                DateTime endDt = wDt.AddDays(6);
+                var act = wr.Action;
+                if (act.StatusId==4&& act.CompleteTime != null && act.CompleteTime.Value.Date <= endDt.Date) {
+                    wr.IsCompletedInWeek = true;
+                }
+            }
+            MainViewModel.DataProvider.SaveChanges();
         }
         ObservableCollection<WeekRecord> weekRecords;
         public ObservableCollection<WeekRecord> WeekRecords {
@@ -41,11 +66,13 @@ namespace ListOfDeal.Classes {
             }
         }
 
+   
+
         void CreateItems() {
             var activeActions = MainViewModel.DataProvider.GetActions().Where(x => x.Project.StatusId == 1 && x.IsActive && x.StatusId != 4).ToList();
             var wRecords = MainViewModel.DataProvider.GetWeekRecords();
             foreach (var act in activeActions) {
-                string weekId= DateTime.Today.ToString("MMddyyyy"); ;
+                string weekId= DateTime.Today.AddDays(1).ToString("MMddyyyy"); ;
                 var cnt = wRecords.Where(x => x.WeekId == weekId && x.ActionId == act.Id).Count();
                 if (cnt > 0)
                     return;
@@ -57,8 +84,6 @@ namespace ListOfDeal.Classes {
             }
             MainViewModel.DataProvider.SaveChanges();
         }
-        void SetIsCompleted() {
-
-        }
+      
     }
 }

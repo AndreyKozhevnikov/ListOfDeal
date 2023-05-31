8a90cbe70d8a6e066a21651fd442fd1b6665867d
kozhevnikov.andrey <andrey.kozhevnikov@devexpress.com>
2023-05-31 14:03:06 +0400

    connection string

--- ListOfDeal/Properties/AssemblyInfo.cs
+++ ListOfDeal/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("23.5.18.1514")]
-[assembly: AssemblyVersion("23.5.18.1514")]
-[assembly: AssemblyFileVersion("23.5.18.1514")]
+// [assembly: AssemblyVersion("23.5.20.1303")]
+[assembly: AssemblyVersion("23.5.20.1303")]
+[assembly: AssemblyFileVersion("23.5.20.1303")]
